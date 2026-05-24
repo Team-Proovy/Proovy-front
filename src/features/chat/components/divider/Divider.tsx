c5e130edfea2b6b3c9cc7e0ecb5779d3f4f7aecf
stringnine <alexjee85@gmail.com>
2026-05-24 14:03:01 +0900

    feat: 뷰어 좌우 이동 커서 영역 추가 (#276)

--- src/features/chat/components/divider/Divider.tsx
+++ src/features/chat/components/divider/Divider.tsx
@@ -15,17 +15,21 @@ export const Divider = ({
   isDragging,
 }: DividerProps) => {
   return (
-    <div className="relative z-10 flex w-0 items-center justify-center">
-      {/* 드래그 핸들 버튼 - 경계선 중심에 떠있는 형태 */}
+    <div className="relative z-10 flex h-full w-0 items-center justify-center">
+      {/* 세로 경계선 전체를 드래그 hit area로 사용 */}
       <div
         onMouseDown={onMouseDown}
         onTouchStart={onTouchStart}
-        className={`absolute flex h-[56px] w-[24px] cursor-col-resize touch-none flex-col items-center justify-center gap-[3px] rounded-[20px] border-[0.5px] border-[#D1D6DE] bg-white shadow-[4px_4px_15px_0px_rgba(0,0,0,0.1)] transition-all hover:scale-105 ${
-          isDragging ? "scale-105" : ""
-        }`}
+        className="group absolute inset-y-0 left-1/2 flex w-[24px] -translate-x-1/2 cursor-col-resize touch-none items-center justify-center"
       >
-        <ArrowLeftIcon size={14} />
-        <ArrowRightIcon size={14} />
+        <div
+          className={`flex h-[56px] w-[24px] flex-col items-center justify-center gap-[3px] rounded-[20px] border-[0.5px] border-[#D1D6DE] bg-white shadow-[4px_4px_15px_0px_rgba(0,0,0,0.1)] transition-all group-hover:scale-105 ${
+            isDragging ? "scale-105" : ""
+          }`}
+        >
+          <ArrowLeftIcon size={14} />
+          <ArrowRightIcon size={14} />
+        </div>
       </div>
     </div>
   );
